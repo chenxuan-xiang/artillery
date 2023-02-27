5191759143e81d12f963af71a6a3cf3b44f69310
chenxuan xiang <xcx_shine@163.com>
2023-02-27 14:13:21 -0500

    guoran

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -5,7 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
 const port = process.env.PORT || 8000
-const socketPath = `https://artillery.herokuapp.com/`
+const socketPath = `http://localhost:${port}`
 
 const dimension = {
   backgroundWidth:1000,
