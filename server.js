ee7ffd31f916c2d2791076a84c16e6d480f6db6a
chenxuan xiang <xcx_shine@163.com>
2023-02-27 13:16:56 -0500

    sdf

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const server = http.createServer(app);
 const gameMaster = new GameMaster(server);
 const port = process.env.PORT || 8000;
 
-// const buildPath = path.resolve(__dirname, '..')
-app.use(express.static(path.join(__dirname, "build")));
+const buildPath = path.resolve(__dirname, '..')
+app.use(express.static(path.join(buildPath, "build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(buildPath, "build", "index.html"));
 });
 
 server.listen(port, () => {
